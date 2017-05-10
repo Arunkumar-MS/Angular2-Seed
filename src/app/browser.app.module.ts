2d562bd39c2858e5d546d972349fce228441d373
Rakesh Nayak <rakesh.nayak@in.tesco.com>
2017-05-10 19:20:56 +0530

    Include StoreDevtoolsModule

--- src/app/browser.app.module.ts
+++ src/app/browser.app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { rootReducer } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
     imports: [
       BrowserModule,
-      StoreModule.provideStore(rootReducer)
+      StoreModule.provideStore(rootReducer),
+      StoreDevtoolsModule.instrumentOnlyWithExtension({
+        maxAge: 10
+      })
     ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
--- src/app/server.app.module.ts
+++ src/app/server.app.module.ts
@@ -5,11 +5,15 @@ import { UniversalModule } from 'angular2-universal/node';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { rootReducer } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
     imports: [
       UniversalModule,
-      StoreModule.provideStore(rootReducer)
+      StoreModule.provideStore(rootReducer),
+      StoreDevtoolsModule.instrumentOnlyWithExtension({
+        maxAge: 10
+      })
     ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
