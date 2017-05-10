2a538f450338d2a3797c8710974bc4b95af24667
Rakesh Nayak <rakesh.nayak@in.tesco.com>
2017-05-10 20:30:45 +0530

    added app.import.ts

--- src/app/browser.app.module.ts
+++ src/app/browser.app.module.ts
@@ -3,18 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { rootReducer } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { APP_PROVIDERS } from './app.providers';
+import { APP_IMPORTS } from './app.imports';
 
 @NgModule({
     imports: [
       BrowserModule,
-      StoreModule.provideStore(rootReducer),
-      StoreDevtoolsModule.instrumentOnlyWithExtension({
-        maxAge: 10
-      })
+      ...APP_IMPORTS
     ],
     providers: APP_PROVIDERS,
     declarations: [AppComponent],
--- src/app/server.app.module.ts
+++ src/app/server.app.module.ts
@@ -3,18 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal/node';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { rootReducer } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { APP_PROVIDERS } from './app.providers';
-
+import { APP_IMPORTS } from './app.imports';
 @NgModule({
     imports: [
       UniversalModule,
-      StoreModule.provideStore(rootReducer),
-      StoreDevtoolsModule.instrumentOnlyWithExtension({
-        maxAge: 10
-      })
+      ...APP_IMPORTS
     ],
     providers: APP_PROVIDERS,
     declarations: [AppComponent],
